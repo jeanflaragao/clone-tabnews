af2fd33eb74b68f8721ccfc07db0bc72f32a7410
Jean Aragão <aragao.jean@gmail.com>
2025-10-16 01:34:27 +0000

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({message:"Students from curso.dev are awesome!"});
+  const dataBaseVersionResult = await database.query("SHOW server_version;");
+  const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version;
+
+  const dataBaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const dataBaseMaxConnectionsValue =
+    dataBaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dataBaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
+
+  const dataBaseOpenedConnectionsValue =
+    dataBaseOpenedConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    update_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        version: dataBaseVersionValue,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        opened_connections: parseInt(dataBaseOpenedConnectionsValue),
+      },
+    },
+  });
 }
 
 export default status;
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET /api/v1/status should returns status 200 ", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+  const parsedDate = Date.parse(responseBody.update_at);
+  expect(responseBody.update_at).toBe(new Date(parsedDate).toISOString());
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
