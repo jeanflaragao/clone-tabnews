8948d962e07bb0a151667f976c8cc37e59c91ceb
Jean Aragão <aragao.jean@gmail.com>
2025-12-23 00:37:57 +0000

    fix: format JSX return statement for better readability

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-  return <h1>Hi, I am studying with Deschamps and it is my first deployment</h1>;
+  return (
+    <h1>Hi, I am studying with Deschamps and it is my first deployment</h1>
+  );
 }
 
 export default Home;
