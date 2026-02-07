9386c8d4b618133f41c0b6734e0e87724bb306c1
Jean Aragão <aragao.jean@gmail.com>
2026-02-07 14:17:46 +0000

    feat: refactor migration and status API to use next-connect router and improve error handling

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,42 +1,38 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from 'infra/errors';
+import controller from "infra/controller.js";
 
-async function status(req, res) {
-  try {
-    const dataBaseVersionResult = await database.query("SHOW server_version;");
-    const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version;
+const router = createRouter();
+router.get(getHandler);
+export default router.handler(controller.errorHandlers);
 
-    const dataBaseMaxConnectionsResult = await database.query(
-      "SHOW max_connections;",
-    );
-    const dataBaseMaxConnectionsValue =
-      dataBaseMaxConnectionsResult.rows[0].max_connections;
+async function getHandler(req, res) {
+  const dataBaseVersionResult = await database.query("SHOW server_version;");
+  const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version;
 
-    const databaseName = process.env.POSTGRES_DB;
-    const dataBaseOpenedConnectionsResult = await database.query({
-      text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1;",
-      values: [databaseName],
-    });
+  const dataBaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const dataBaseMaxConnectionsValue =
+    dataBaseMaxConnectionsResult.rows[0].max_connections;
 
-    const dataBaseOpenedConnectionsValue =
-      dataBaseOpenedConnectionsResult.rows[0].count;
+  const databaseName = process.env.POSTGRES_DB;
+  const dataBaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
 
-    res.status(200).json({
-      update_at: new Date().toISOString(),
-      dependencies: {
-        database: {
-          version: dataBaseVersionValue,
-          max_connections: parseInt(dataBaseMaxConnectionsValue),
-          opened_connections: parseInt(dataBaseOpenedConnectionsValue),
-        },
+  const dataBaseOpenedConnectionsValue =
+    dataBaseOpenedConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    update_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        version: dataBaseVersionValue,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        opened_connections: parseInt(dataBaseOpenedConnectionsValue),
       },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({ cause: error });
-    console.log("\n Error on catch controller: ");
-    console.error(publicErrorObject);
-    res.status(500).json({ error: "Internal server error" });
-  }
+    },
+  });
 }
-
-export default status;
